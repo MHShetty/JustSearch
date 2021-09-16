8c5472ae8f88151c931bfc2924e095a0a7a7d7ae
MHShetty <shettymohitn@gmail.com>
2021-09-16 14:56:19 +0530

    Properly support multiple widgets

--- app/src/main/java/app/grapheneos/searchbar/android/provider/HomeScreenWidgetProvider.kt
+++ app/src/main/java/app/grapheneos/searchbar/android/provider/HomeScreenWidgetProvider.kt
@@ -11,11 +11,11 @@ import app.grapheneos.searchbar.android.SharedPreferencesPersistedValues
 import app.grapheneos.searchbar.android.activity.AssistActivity
 import app.grapheneos.searchbar.arch.model.generateProviders
 import android.content.SharedPreferences
+import app.grapheneos.searchbar.arch.model.SearchProvider
 
 class HomeScreenWidgetProvider : AppWidgetProvider() {
 
     private lateinit var context: Context
-    private var appWidgetId: Int = 0
 
     override fun onUpdate(context: Context?, appWidgetManager: AppWidgetManager?, appWidgetIds: IntArray?) {
         super.onUpdate(context, appWidgetManager, appWidgetIds)
@@ -24,44 +24,44 @@ class HomeScreenWidgetProvider : AppWidgetProvider() {
 
         val appWidgetLayout = RemoteViews(context.packageName, R.layout.activity_assist_preview)
 
-        appWidgetLayout.setImageViewResource(R.id.providerButton, findSelectedProvider().iconRes())
-        appWidgetLayout.setTextViewText(
-            R.id.searchHint, "${findSelectedProvider().name()} Search"
-        )
-
         val intent = Intent(context, AssistActivity::class.java)
         val pendingIntent = PendingIntent.getActivity(context,0, intent, 0)
 
-        appWidgetLayout.setOnClickPendingIntent(R.id.container, pendingIntent)
         appWidgetIds?.let {
             for (appWidgetId in it) {
+                appWidgetLayout.setImageViewResource(R.id.providerButton,
+                    findSelectedProvider(appWidgetId).iconRes())
+                appWidgetLayout.setTextViewText(
+                    R.id.searchHint,
+                    "${findSelectedProvider(appWidgetId).name()} Search")
+
+                appWidgetLayout.setOnClickPendingIntent(R.id.container, pendingIntent)
                 appWidgetManager?.updateAppWidget(appWidgetId, appWidgetLayout)
             }
         }
-
-        appWidgetId = appWidgetIds?.firstOrNull()!!
     }
 
-    private fun findSelectedProvider() =
-        findProvider(persistedValues.getPersistedProviderId())
+    private fun findSelectedProvider(appWidgetId: Int) =
+        findProvider(persistedValuesFor(appWidgetId).getPersistedProviderId(), appWidgetId)
 
     private fun getSharedPreferencesNameForAppWidget(context: Context, appWidgetId: Int): String {
         return context.packageName + "_preferences_" + appWidgetId
     }
 
-    private fun getPreferences(): SharedPreferences? {
-
+    private fun getPreferences(appWidgetId: Int): SharedPreferences? {
         return context.getSharedPreferences(
             getSharedPreferencesNameForAppWidget(context, appWidgetId), 0
         )
     }
 
-    private val persistedValues by lazy {
-        SharedPreferencesPersistedValues(context, getPreferences()!!)
+    private fun persistedValuesFor(appWidgetId: Int) : SharedPreferencesPersistedValues =
+        SharedPreferencesPersistedValues(context, getPreferences(appWidgetId)!!)
+
+    private fun providersFor(appWidgetId: Int) =
+        generateProviders(context, persistedValuesFor(appWidgetId))
+
+    private fun findProvider(providerId: Long, appWidgetId: Int) : SearchProvider {
+        val providers = providersFor(appWidgetId)
+        return providers.firstOrNull { it.id() == providerId } ?: providers[0]
     }
-
-    private val providers by lazy { generateProviders(context, persistedValues) }
-
-    private fun findProvider(providerId: Long) =
-        providers.firstOrNull { it.id() == providerId } ?: providers[0]
 }
