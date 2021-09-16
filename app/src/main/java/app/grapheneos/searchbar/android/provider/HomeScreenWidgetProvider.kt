226e8e4fa10ae081091e6cf8575ab149e1c34353
MHShetty <shettymohitn@gmail.com>
2021-09-16 13:17:46 +0530

    Update icon of search bar (based on preferences)

--- app/src/main/java/app/grapheneos/searchbar/android/provider/HomeScreenWidgetProvider.kt
+++ app/src/main/java/app/grapheneos/searchbar/android/provider/HomeScreenWidgetProvider.kt
@@ -7,13 +7,25 @@ import android.content.Context
 import android.content.Intent
 import android.widget.RemoteViews
 import app.grapheneos.searchbar.R
+import app.grapheneos.searchbar.android.SharedPreferencesPersistedValues
 import app.grapheneos.searchbar.android.activity.AssistActivity
+import app.grapheneos.searchbar.arch.model.generateProviders
+import android.content.SharedPreferences
 
 class HomeScreenWidgetProvider : AppWidgetProvider() {
+
+    private lateinit var context: Context
+    private var appWidgetId: Int = 0
+
     override fun onUpdate(context: Context?, appWidgetManager: AppWidgetManager?, appWidgetIds: IntArray?) {
         super.onUpdate(context, appWidgetManager, appWidgetIds)
 
-        val appWidgetLayout = RemoteViews(context?.packageName, R.layout.activity_assist_preview)
+        this.context = context!!
+
+        val appWidgetLayout = RemoteViews(context.packageName, R.layout.activity_assist_preview)
+
+        appWidgetLayout.setImageViewResource(R.id.providerButton, findSelectedProvider().iconRes())
+
         val intent = Intent(context, AssistActivity::class.java)
         val pendingIntent = PendingIntent.getActivity(context,0, intent, 0)
 
@@ -23,5 +35,30 @@ class HomeScreenWidgetProvider : AppWidgetProvider() {
                 appWidgetManager?.updateAppWidget(appWidgetId, appWidgetLayout)
             }
         }
+
+        appWidgetId = appWidgetIds?.firstOrNull()!!
     }
+
+    private fun findSelectedProvider() =
+        findProvider(persistedValues.getPersistedProviderId())
+
+    private fun getSharedPreferencesNameForAppWidget(context: Context, appWidgetId: Int): String {
+        return context.packageName + "_preferences_" + appWidgetId
+    }
+
+    private fun getPreferences(): SharedPreferences? {
+
+        return context.getSharedPreferences(
+            getSharedPreferencesNameForAppWidget(context, appWidgetId), 0
+        )
+    }
+
+    private val persistedValues by lazy {
+        SharedPreferencesPersistedValues(context, getPreferences()!!)
+    }
+
+    private val providers by lazy { generateProviders(context, persistedValues) }
+
+    private fun findProvider(providerId: Long) =
+        providers.firstOrNull { it.id() == providerId } ?: providers[0]
 }
